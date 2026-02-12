bd9190b77373a694ff04e64eec1655087dbebceb
Alik Hansen <Ahansen32@my.wctc.edu>
2026-02-12 14:24:10 -0600

    [1.08] Read data from file

--- Program.cs
+++ Program.cs
@@ -23,6 +23,25 @@ else
     List<string> Species = [];
     List<string> FirstAppearance = [];
     List<string> YearCreated = [];
+
+    // to populate the lists with data, read from the data file
+    try
+    {
+        StreamReader sr = new(file);
+        // first line contains column headers
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        sr.Close();
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex.Message);
+    }
+
        string? choice;
     do
     {
